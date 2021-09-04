8e3b24a31daae5fa950646891078a30df682551b
Nic Fellows <1664536+nicf82@users.noreply.github.com>
2021-09-04 21:07:03 +0100

    More debugging and rewriting

---
+++ run_javacpc.sh
@@ -0,0 +1,3 @@
+D=$(pwd)
+cd /Users/nic/Downloads/JavaCPC_Desktop_2.9.8f/
+java -jar JavaCPC.jar --cpctype CPC464 --drivea ${D}/life.dsk --autotype 'run"life'
--- src/main.c
+++ src/main.c
@@ -55,7 +55,7 @@ uint8_t board2[BD_W][BD_H];
 #define SCR_TO_BD(x) x-1
 
 #define IS_DIRTY(x) x & (uint8_t)1
-#define IS_ALIVE(x) x & (uint8_t)2
+#define IS_ALIVE(x) (x & (uint8_t)2)
 #define IS_NOT_ALIVE(x) !(x & (uint8_t)2)
 
 #define IS_DIRTY_0(x) x & (uint8_t)1
@@ -85,15 +85,18 @@ void debug_str_uint8(char *s, uint8_t i) {
 
 void debug_board(uint8_t board[BD_W][BD_H]) {
    for (uint8_t y = 0; y < BD_H; y++) {
-      printf("%02d:");
+      printf("%02d:", y);
       for(uint8_t x = 0; x < BD_W; x++) {
+         if(board[x][y]) inverse();
          printf(" %x", board[x][y]);
+         if(board[x][y]) inverse();
       }
       cputs("\r\n");
    }
 }
 
-inline uint8_t * point_to_board_cellgp_ref(uint8_t board[BD_W][BD_H], struct Point *p) {
+//Note - this broke when i made it inline
+uint8_t * point_to_board_cellgp_ref(uint8_t board[BD_W][BD_H], struct Point *p) {
    return &board[(p->x-1) / 4][p->y-1];
 }
 
@@ -123,7 +126,107 @@ void put_on_board(uint8_t board[BD_W][BD_H], struct Point *shape, uint8_t len, u
    }
 }
 
-uint8_t live_neigbs_cnt(uint8_t board[BD_W][BD_H], struct Point *p, uint8_t baseshift) {
+#define UL(nbr, p) nbr.x = p->x-1; nbr.y = p->y-1
+#define UC(nbr, p) nbr.x = p->x;   nbr.y = p->y-1
+#define UR(nbr, p) nbr.x = p->x+1; nbr.y = p->y-1
+
+#define CL(nbr, p) nbr.x = p->x-1; nbr.y = p->y
+#define CR(nbr, p) nbr.x = p->x+1; nbr.y = p->y
+
+#define LL(nbr, p) nbr.x = p->x-1; nbr.y = p->y+1
+#define LC(nbr, p) nbr.x = p->x;   nbr.y = p->y+1
+#define LR(nbr, p) nbr.x = p->x+1; nbr.y = p->y+1
+
+#define SH_DN(s) (s==0 ? 6 : s-2)
+#define SH_UP(s) (s==6 ? 0 : s+2)
+
+uint8_t live_neigbs_cnt(uint8_t board[BD_W][BD_H], struct Point *p, uint8_t shift) {
+
+   uint8_t c = 0, s, *cellgp, start = 0, end = 8;
+   struct Point nbr;
+
+   if(p->x > X_MIN && p->x < X_MAX && p->y > Y_MIN && p->y < Y_MAX) {
+
+      // printf("--Checking nbrs of (%02d,%02d)[%d]\r\n", p->x, p->y, shift);
+
+      UL(nbr, p);
+      cellgp = point_to_board_cellgp_ref(board, &nbr);
+      // printf("Value of cellgp at (%02d,%02d) is %02x\r\n", nbr.x, nbr.y, *cellgp);
+      s = SH_DN(shift);
+      if(IS_ALIVE(*cellgp >> s)) {
+         // printf("(%02d,%02d)[%d] was alive\r\n", nbr.x, nbr.y, s);
+         c++;
+      }
+
+      UC(nbr, p);
+      // printf("Value of cellgp at (%02d,%02d) is %02x\r\n", nbr.x, nbr.y, *cellgp);
+      cellgp = point_to_board_cellgp_ref(board, &nbr);
+      if(IS_ALIVE(*cellgp >> shift)) {
+         // printf("(%02d,%02d)[%d] was alive\r\n", nbr.x, nbr.y, s);
+         c++;
+      }
+
+      UR(nbr, p);
+      cellgp = point_to_board_cellgp_ref(board, &nbr);
+      // printf("Value of cellgp at (%02d,%02d) is %02x\r\n", nbr.x, nbr.y, *cellgp);
+      s = SH_UP(shift);
+      if(IS_ALIVE(*cellgp >> s)) {
+         // printf("(%02d,%02d)[%d] was alive\r\n", nbr.x, nbr.y, s);
+         c++;
+      }
+
+      CL(nbr, p);
+      cellgp = point_to_board_cellgp_ref(board, &nbr);
+      // printf("Value of cellgp at (%02d,%02d) is %02x\r\n", nbr.x, nbr.y, *cellgp);
+      s = SH_DN(shift);
+      if(IS_ALIVE(*cellgp >> s)) {
+         // printf("(%02d,%02d)[%d] was alive\r\n", nbr.x, nbr.y, s);
+         c++;
+      }
+
+      CR(nbr, p);
+      cellgp = point_to_board_cellgp_ref(board, &nbr);
+      // printf("Value of cellgp at (%02d,%02d) is %02x\r\n", nbr.x, nbr.y, *cellgp);
+      s = SH_UP(shift);
+      if(IS_ALIVE(*cellgp >> s)) {
+         // printf("(%02d,%02d)[%d] was alive\r\n", nbr.x, nbr.y, s);
+         c++;
+      }
+
+      LL(nbr, p);
+      cellgp = point_to_board_cellgp_ref(board, &nbr);
+      // printf("Value of cellgp at (%02d,%02d) is %02x\r\n", nbr.x, nbr.y, *cellgp);
+      s = SH_DN(shift);
+      if(IS_ALIVE(*cellgp >> s)) {
+         // printf("(%02d,%02d)[%d] was alive\r\n", nbr.x, nbr.y, s);
+         c++;
+      }
+
+      LC(nbr, p);
+      // printf("Value of cellgp at (%02d,%02d) is %02x\r\n", nbr.x, nbr.y, *cellgp);
+      cellgp = point_to_board_cellgp_ref(board, &nbr);
+      if(IS_ALIVE(*cellgp >> shift)) {
+         // printf("(%02d,%02d)[%d] was alive\r\n", nbr.x, nbr.y, s);
+         c++;
+      }
+
+      LR(nbr, p);
+      cellgp = point_to_board_cellgp_ref(board, &nbr);
+      // printf("Value of cellgp at (%02d,%02d) is %02x\r\n", nbr.x, nbr.y, *cellgp);
+      s = SH_UP(shift);
+      if(IS_ALIVE(*cellgp >> s)) {
+         // printf("(%02d,%02d)[%d] was alive\r\n", nbr.x, nbr.y, s);
+         c++;
+      }
+   }
+
+   cputcxy(p->x, p->y, c+ASCII_0);
+
+   return c;
+}
+
+
+uint8_t live_neigbs_cnt_old(uint8_t board[BD_W][BD_H], struct Point *p, uint8_t baseshift) {
 
    uint8_t c = 0, *cellgp, shift, start = 0, end = 8;
    struct Point all[] = { {p->x-1, p->y-1}, {p->x, p->y-1}, {p->x+1, p->y-1}, 
@@ -144,10 +247,8 @@ uint8_t live_neigbs_cnt(uint8_t board[BD_W][BD_H], struct Point *p, uint8_t base
    // baseshift = ((p->x-1)%4)*2;
    
    for(uint8_t i = start; i <= end; i++) {
-      if(all[i].x==0) {
-         printf("X was 0 for i:%d, p(%d,%d)\r\n", i, p->x, p->y);
-         while(true);
-      }
+      if(all[i].x==0)
+         continue;
 
 
 
@@ -176,7 +277,7 @@ uint8_t live_neigbs_cnt(uint8_t board[BD_W][BD_H], struct Point *p, uint8_t base
 
 void evolve(uint8_t src_board[BD_W][BD_H], uint8_t dst_board[BD_W][BD_H]) {
    struct Point p = {0,0};
-   uint8_t *cellgp, c;
+   uint8_t *dst_cellgp, *src_cellgp, c;
 
    cputcxy(1, 1, '.');
    memcpy(dst_board, src_board, BD_W*BD_H);
@@ -185,38 +286,39 @@ void evolve(uint8_t src_board[BD_W][BD_H], uint8_t dst_board[BD_W][BD_H]) {
    for (p.y = Y_MIN; p.y <= Y_MAX; p.y++) {
       for(p.x = X_MIN; p.x <= X_MAX; ) {
 
-         cellgp = point_to_board_cellgp_ref(dst_board, &p);
+         src_cellgp = point_to_board_cellgp_ref(src_board, &p);
+         dst_cellgp = point_to_board_cellgp_ref(dst_board, &p);
 
          //Evolve cell 0
          c = live_neigbs_cnt(src_board, &p, 0);
-         if(c == 3 && IS_NOT_ALIVE_0(*cellgp))
-            update_cellgp(cellgp, &p, 0, true);
-         else if (c != 2 && c != 3 && IS_ALIVE_0(*cellgp))
-            update_cellgp(cellgp, &p, 0, false);
+         if(c == 3 && IS_NOT_ALIVE_0(*src_cellgp))
+            update_cellgp(dst_cellgp, &p, 0, true);
+         else if (c != 2 && c != 3 && IS_ALIVE_0(*src_cellgp))
+            update_cellgp(dst_cellgp, &p, 0, false);
          p.x++;
 
          //Evolve cell 1
          c = live_neigbs_cnt(src_board, &p, 2);
-         if(c == 3 && IS_NOT_ALIVE_1(*cellgp))
-            update_cellgp(cellgp, &p, 2, true);
-         else if (c != 2 && c != 3 && IS_ALIVE_1(*cellgp))
-            update_cellgp(cellgp, &p, 2, false);
+         if(c == 3 && IS_NOT_ALIVE_1(*src_cellgp))
+            update_cellgp(dst_cellgp, &p, 2, true);
+         else if (c != 2 && c != 3 && IS_ALIVE_1(*src_cellgp))
+            update_cellgp(dst_cellgp, &p, 2, false);
          p.x++;
 
          //Evolve cell 2
          c = live_neigbs_cnt(src_board, &p, 4);
-         if(c == 3 && IS_NOT_ALIVE_2(*cellgp))
-            update_cellgp(cellgp, &p, 4, true);
-         else if (c != 2 && c != 3 && IS_ALIVE_2(*cellgp))
-            update_cellgp(cellgp, &p, 4, false);
+         if(c == 3 && IS_NOT_ALIVE_2(*src_cellgp))
+            update_cellgp(dst_cellgp, &p, 4, true);
+         else if (c != 2 && c != 3 && IS_ALIVE_2(*src_cellgp))
+            update_cellgp(dst_cellgp, &p, 4, false);
          p.x++;
 
          //Evolve cell 3
          c = live_neigbs_cnt(src_board, &p, 6);
-         if(c == 3 && IS_NOT_ALIVE_3(*cellgp))
-            update_cellgp(cellgp, &p, 6, true);
-         else if (c != 2 && c != 3 && IS_ALIVE_3(*cellgp))
-            update_cellgp(cellgp, &p, 6, false);
+         if(c == 3 && IS_NOT_ALIVE_3(*src_cellgp))
+            update_cellgp(dst_cellgp, &p, 6, true);
+         else if (c != 2 && c != 3 && IS_ALIVE_3(*src_cellgp))
+            update_cellgp(dst_cellgp, &p, 6, false);
          p.x++;
          
       }
@@ -227,17 +329,53 @@ void evolve(uint8_t src_board[BD_W][BD_H], uint8_t dst_board[BD_W][BD_H]) {
 
 void main(void) {
 
+   struct Point square[] = {{0,0}};
+   struct Point p = {5,5};
+
    bordercolor(0);
 
    memset(&board1, 0, BD_W*BD_H);
    memset(&board2, 0, BD_W*BD_H);
 
-   // put_on_board(board1, glider, 5, 4, 4);
-   put_on_board(board1, gospel, 36, 1, 1);
+   put_on_board(board1, square, 1, 2, 2);
+   //put_on_board(board1, glider, 5, 6, 6);
+   //put_on_board(board1, gospel, 36, 1, 1);
 
-   while(true) {
-      evolve(board1, board2);   
-      evolve(board2, board1);   
+   // debug_board(board1);
+
+   for (p.y = Y_MIN; p.y <= Y_MAX; p.y++) {
+      for(p.x = X_MIN; p.x <= X_MAX; ) {
+
+         //Evolve cell 0
+         live_neigbs_cnt(board1, &p, 0);
+         p.x++;
+
+         //Evolve cell 1
+         live_neigbs_cnt(board1, &p, 2);
+         p.x++;
+
+         //Evolve cell 2
+         live_neigbs_cnt(board1, &p, 4);
+         p.x++;
+
+         //Evolve cell 3
+         live_neigbs_cnt(board1, &p, 6);
+         p.x++;
+         
+      }
    }
+
+   //debug_board(board1);
+
+   // evolve(board1, board2); 
+   
+   // debug_board(board2);
+
+   // while(true) {
+   //    evolve(board1, board2);   
+   //    evolve(board2, board1);   
+   // }
+
+   while(true);
 }
 
