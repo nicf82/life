69fea671e1926b577fa75ef2519a528ab4a2ff4f
Nic Fellows <1664536+nicf82@users.noreply.github.com>
2021-09-05 22:23:36 +0100

    Use aligned memory to allow for easier neighbour arithmetic

--- src/main.c
+++ src/main.c
@@ -45,38 +45,36 @@ const struct Point gospel[] = {
 #define Y_MIN 1
 #define Y_MAX 25
 
-#define BD_W X_MAX/4
+//New board stuff
+#define BUF_WIDTH 0x0010 //16 addresses
+#define BUF_WIDTH_MSK BUF_WIDTH-1
+#define GET_CELL(board, p) board + (BUF_WIDTH*(p.y-1)) + ((p.x-1)/4)
+
+#define BD_DATA_W X_MAX/4  //The buffer row width is 16, but only 10 bits are used
 #define BD_H Y_MAX
+#define BD_BUFSIZ BUF_WIDTH*BD_H*2+BUF_WIDTH_MSK //Enough for 2 boards to align to a 64bit boundary
 
-uint8_t board1[BD_W][BD_H];
-uint8_t board2[BD_W][BD_H];
+uint8_t boardbuffer[BD_BUFSIZ];
 
-#define SCR_TO_BD(x) x-1
-
-#define IS_DIRTY(x) x & (uint8_t)1
 #define IS_ALIVE(x) (x & (uint8_t)2)
 #define IS_NOT_ALIVE(x) !(x & (uint8_t)2)
 
-#define IS_DIRTY_0(x) x & (uint8_t)1
 #define IS_ALIVE_0(x) x & (uint8_t)2
 #define IS_NOT_ALIVE_0(x) !(x & (uint8_t)2)
 
-#define IS_DIRTY_1(x) x & (uint8_t)4
 #define IS_ALIVE_1(x) x & (uint8_t)8
 #define IS_NOT_ALIVE_1(x) !(x & (uint8_t)8)
 
-#define IS_DIRTY_2(x) x & (uint8_t)16
 #define IS_ALIVE_2(x) x & (uint8_t)32
 #define IS_NOT_ALIVE_2(x) !(x & (uint8_t)32)
 
-#define IS_DIRTY_3(x) x & (uint8_t)64
 #define IS_ALIVE_3(x) x & (uint8_t)128
 #define IS_NOT_ALIVE_3(x) !(x & (uint8_t)128)
 
 #define ASCII_0 48
 #define ASCII_a 32
 
-#define GET_CELLGP_REF(board, p) &(board[(p.x-1) / 4][p.y-1])
+#define DRAW 1
 
 void debug_str_uint8(char *s, uint8_t i) {
    cputs(s);
@@ -84,13 +82,15 @@ void debug_str_uint8(char *s, uint8_t i) {
    cputs("\r\n");
 }
 
-void debug_board(uint8_t board[BD_W][BD_H]) {
+void debug_board(uint8_t * board) {
+   uint8_t *cell;
    for (uint8_t y = 0; y < BD_H; y++) {
       printf("%02d:", y);
-      for(uint8_t x = 0; x < BD_W; x++) {
-         if(board[x][y]) inverse();
-         printf(" %x", board[x][y]);
-         if(board[x][y]) inverse();
+      for(uint8_t x = 0; x < BD_DATA_W; x++) {
+         cell = board + (BUF_WIDTH*y) + x;
+         if(*cell) inverse();
+         printf(" %02x", *cell);
+         if(*cell) inverse();
       }
       cputs("\r\n");
    }
@@ -101,24 +101,28 @@ void update_cellgp(uint8_t *cellgp, struct Point *p, uint8_t shift, bool set) {
    *cellgp = *cellgp | (1 << shift); //Set dirty flag
    if(set) {
       *cellgp = *cellgp | (2 << shift); //Set alive
-      cputcxy(p->x, p->y, 0xCE);
+      #if DRAW
+         cputcxy(p->x, p->y, 0xCE);
+      #endif
    }
    else {
       *cellgp = *cellgp & ~(2 << shift); //Set not alive &= ~(1UL << n);
-      cputcxy(p->x, p->y, ' ');
+      #if DRAW
+         cputcxy(p->x, p->y, ' ');
+      #endif
    }
 }
 
-void put_on_board(uint8_t board[BD_W][BD_H], struct Point *shape, uint8_t len, uint8_t ox, uint8_t oy) {
-   uint8_t *cellgp, shift;
+void put_on_board(uint8_t * board, struct Point *shape, uint8_t len, uint8_t ox, uint8_t oy) {
+   uint8_t *cell, shift;
    struct Point p;
    for(uint8_t i = 0; i < len; i++) {
       p.x = ox+shape[i].x;
       p.y = oy+shape[i].y;
 
-      cellgp = GET_CELLGP_REF(board, p);
+      cell = GET_CELL(board, p);
       shift = ((p.x-1)%4)*2;
-      update_cellgp(cellgp, &p, shift, true);
+      update_cellgp(cell, &p, shift, true);
    }
 }
 
@@ -136,9 +140,9 @@ void put_on_board(uint8_t board[BD_W][BD_H], struct Point *shape, uint8_t len, u
 #define SH_DN(s) (s==0 ? 6 : s-2)
 #define SH_UP(s) (s==6 ? 0 : s+2)
 
-uint8_t live_neigbs_cnt(uint8_t board[BD_W][BD_H], struct Point *p, uint8_t shift) {
+uint8_t live_neigbs_cnt(uint8_t *board, struct Point *p, uint8_t shift) {
 
-   uint8_t c = 0, *cellgp, start = 0, end = 8;
+   uint8_t c = 0, *cell, start = 0, end = 8;
    struct Point nbr;
 
    uint8_t sh_up = SH_UP(shift);
@@ -146,235 +150,230 @@ uint8_t live_neigbs_cnt(uint8_t board[BD_W][BD_H], struct Point *p, uint8_t shif
 
    if(p->y == Y_MIN) {
       LC(nbr, p);
-      cellgp = GET_CELLGP_REF(board, nbr);
-      if(IS_ALIVE(*cellgp >> shift)) c++;
+      cell = GET_CELL(board, nbr);
+      if(IS_ALIVE(*cell >> shift)) c++;
 
       if(p->x == X_MIN) {
          CR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
 
          LR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
       }
 
       if(p->x > X_MIN && p->x < X_MAX) {
          CL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
 
          LL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
 
          CR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
 
          LR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
       }
 
       if(p->x == X_MAX) {
          CL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
 
          LL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
       }
    }
    
    if(p->y > Y_MIN && p->y < Y_MAX) {
       UC(nbr, p);
-      cellgp = GET_CELLGP_REF(board, nbr);
-      if(IS_ALIVE(*cellgp >> shift)) c++;
+      cell = GET_CELL(board, nbr);
+      if(IS_ALIVE(*cell >> shift)) c++;
       
       LC(nbr, p);
-      cellgp = GET_CELLGP_REF(board, nbr);
-      if(IS_ALIVE(*cellgp >> shift)) c++;
+      cell = GET_CELL(board, nbr);
+      if(IS_ALIVE(*cell >> shift)) c++;
 
       if(p->x == X_MIN) {
          UR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
 
          CR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
 
          LR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
       }
       
       if(p->x > X_MIN && p->x < X_MAX) {
          UR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
 
          CR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
 
          LR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
 
          UL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
 
          CL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
 
          LL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
       }
       
       if(p->x == X_MAX) {
          UL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
 
          CL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
 
          LL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
       }
    }
 
    if(p->y == Y_MAX) {
       UC(nbr, p);
-      cellgp = GET_CELLGP_REF(board, nbr);
-      if(IS_ALIVE(*cellgp >> shift)) c++;
+      cell = GET_CELL(board, nbr);
+      if(IS_ALIVE(*cell >> shift)) c++;
  
       if(p->x == X_MIN) {
          UR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
 
          CR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
       }
 
       if(p->x > X_MIN && p->x < X_MAX) {
          UR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
 
          CR(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_up)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_up)) c++;
 
          UL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
 
          CL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
       }
 
       if(p->x == X_MAX) {
          UL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
 
          CL(nbr, p);
-         cellgp = GET_CELLGP_REF(board, nbr);
-         if(IS_ALIVE(*cellgp >> sh_dn)) c++;
+         cell = GET_CELL(board, nbr);
+         if(IS_ALIVE(*cell >> sh_dn)) c++;
       }
    }
 
-   // cputcxy(p->x, p->y, c+ASCII_0);
+   // if(c) {
+   //    nbr.x = p->x; nbr.y = p->y;
+   //    cell = GET_CELL(board, nbr);
+   //    if(IS_ALIVE(*cell >> shift)) inverse();
+   //    cputcxy(p->x, p->y, c+ASCII_0);
+   //    if(IS_ALIVE(*cell >> shift)) inverse();
+   // }
 
    return c;
 }
 
-void evolve(uint8_t src_board[BD_W][BD_H], uint8_t dst_board[BD_W][BD_H]) {
+void evolve(uint8_t *src_board, uint8_t *dst_board) {
    struct Point p = {0,0};
-   uint8_t *dst_cellgp, *src_cellgp, c;
+   uint8_t *dst_cell, *src_cell, c;
 
-   cputcxy(1, 1, '.');
-   memcpy(dst_board, src_board, BD_W*BD_H);
-   cputcxy(1, 1, ' ');
+   memcpy(dst_board, src_board, BUF_WIDTH*BD_H);
 
    for (p.y = Y_MIN; p.y <= Y_MAX; p.y++) {
       for(p.x = X_MIN; p.x <= X_MAX; ) {
 
-         src_cellgp = GET_CELLGP_REF(src_board, p);
-         dst_cellgp = GET_CELLGP_REF(dst_board, p);
+         src_cell = GET_CELL(src_board, p);
+         dst_cell = GET_CELL(dst_board, p);
 
          //Evolve cell 0
          c = live_neigbs_cnt(src_board, &p, 0);
-         if(c == 3 && IS_NOT_ALIVE_0(*src_cellgp))
-            update_cellgp(dst_cellgp, &p, 0, true);
-         else if (c != 2 && c != 3 && IS_ALIVE_0(*src_cellgp))
-            update_cellgp(dst_cellgp, &p, 0, false);
+         if(c == 3 && IS_NOT_ALIVE_0(*src_cell))
+            update_cellgp(dst_cell, &p, 0, true);
+         else if (c != 2 && c != 3 && IS_ALIVE_0(*src_cell))
+            update_cellgp(dst_cell, &p, 0, false);
          p.x++;
 
          //Evolve cell 1
          c = live_neigbs_cnt(src_board, &p, 2);
-         if(c == 3 && IS_NOT_ALIVE_1(*src_cellgp))
-            update_cellgp(dst_cellgp, &p, 2, true);
-         else if (c != 2 && c != 3 && IS_ALIVE_1(*src_cellgp))
-            update_cellgp(dst_cellgp, &p, 2, false);
+         if(c == 3 && IS_NOT_ALIVE_1(*src_cell))
+            update_cellgp(dst_cell, &p, 2, true);
+         else if (c != 2 && c != 3 && IS_ALIVE_1(*src_cell))
+            update_cellgp(dst_cell, &p, 2, false);
          p.x++;
 
          //Evolve cell 2
          c = live_neigbs_cnt(src_board, &p, 4);
-         if(c == 3 && IS_NOT_ALIVE_2(*src_cellgp))
-            update_cellgp(dst_cellgp, &p, 4, true);
-         else if (c != 2 && c != 3 && IS_ALIVE_2(*src_cellgp))
-            update_cellgp(dst_cellgp, &p, 4, false);
+         if(c == 3 && IS_NOT_ALIVE_2(*src_cell))
+            update_cellgp(dst_cell, &p, 4, true);
+         else if (c != 2 && c != 3 && IS_ALIVE_2(*src_cell))
+            update_cellgp(dst_cell, &p, 4, false);
          p.x++;
 
          //Evolve cell 3
          c = live_neigbs_cnt(src_board, &p, 6);
-         if(c == 3 && IS_NOT_ALIVE_3(*src_cellgp))
-            update_cellgp(dst_cellgp, &p, 6, true);
-         else if (c != 2 && c != 3 && IS_ALIVE_3(*src_cellgp))
-            update_cellgp(dst_cellgp, &p, 6, false);
+         if(c == 3 && IS_NOT_ALIVE_3(*src_cell))
+            update_cellgp(dst_cell, &p, 6, true);
+         else if (c != 2 && c != 3 && IS_ALIVE_3(*src_cell))
+            update_cellgp(dst_cell, &p, 6, false);
          p.x++;
          
       }
    }
 }
 
-
 void main(void) {
 
    struct Point square[] = {{0,0}};
    struct Point p = {5,5};
-   uint8_t *b1;
+   uint8_t *board1, *board2;
 
    bordercolor(0);
 
-   memset(&board1, 0, BD_W*BD_H);
-   memset(&board2, 0, BD_W*BD_H);
-
-   //Align to next 64bit boundary
-   b1 = (uint8_t *)(((uint16_t)&board1+63) & ~(0x003f));
-
-
-   printf("board1 addr: %x\r\n", board1);
-   printf("b1     addr: %x\r\n", b1);
-   printf("board2 addr: %x\r\n", board2);
-
-   while(true);
-
-   // put_on_board(board1, square, 1, 2, 2);
-   // put_on_board(board1, glider, 5, 6, 6);
+   //Clear buffer then align 2 boards to next 64bit boundary
+   memset(&boardbuffer, 0, (BUF_WIDTH*BD_H*2)+BUF_WIDTH_MSK);
+   board1 = (uint8_t*)(((uint16_t)&boardbuffer+BUF_WIDTH_MSK) & ~(BUF_WIDTH_MSK));
+   board2 = board1 + BUF_WIDTH*BD_H;
+   
+   // put_on_board(board1, glider, 5, 3, 3);
+   // put_on_board(board1, square, 1, 5, 5);
    put_on_board(board1, gospel, 36, 1, 1);
 
    while(true) {
